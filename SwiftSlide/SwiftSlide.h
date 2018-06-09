ff8919f95dbeda6b06b2efb400972c81923fbcdd
kyaw than <ekyawthan@gmail.com>
2018-06-09 13:32:22 -0400

    Initial Commit

---
+++ SwiftSlide/SwiftSlide.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSlide.h
+//  SwiftSlide
+//
+//  Created by Kyaw Mong on 9/6/18.
+//  Copyright © 2018 meera solutions inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSlide.
+FOUNDATION_EXPORT double SwiftSlideVersionNumber;
+
+//! Project version string for SwiftSlide.
+FOUNDATION_EXPORT const unsigned char SwiftSlideVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSlide/PublicHeader.h>
+
+
